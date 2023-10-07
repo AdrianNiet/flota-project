f3e5f58739ff6f3c48543d305684f88f999dadbb
Nerscy <146065694+Nerscy@users.noreply.github.com>
2023-10-07 12:08:32 +0200

    Optimizacion + comentarios + modo VS IA o PvsP
    MAS CAMBIOS!

--- hundir la flota/funciones.py
+++ hundir la flota/funciones.py
@@ -1,48 +1,56 @@
+#AQUI TENEMOS LAS FUNCIONES QUE COMPONEN NUESTRO JUEGO.
+#Se centraran en crear barcos y disparar.
+
 import numpy as np
 import random
-from clases import *
+import time
+import clases
 
 def crear_barcos_jugador1(campo_jugador1):
-    
+    #Creamos las variables de claes Barco.
+    #Orientacion es un numero aleatorio, las vidas del barco son introducidas mediante un int.
+    #Aqui generamos numero aleatorio.
     orientacion = random.randint(1,4)
-    barco1= Barco(1,orientacion)
+    #Aqui, generamos un barco con 1 vida.
+    barco1= clases.Barco(1,orientacion)
+    #Repetimos para todos los barcos.
     orientacion = random.randint(1,4)
-    barco1_2= Barco(1,orientacion)
+    barco1_2= clases.Barco(1,orientacion)
     orientacion = random.randint(1,4)
-    barco1_3= Barco(1,orientacion)
+    barco1_3= clases.Barco(1,orientacion)
     orientacion = random.randint(1,4)
-    barco1_4= Barco(1,orientacion)
+    barco1_4= clases.Barco(1,orientacion)
     orientacion = random.randint(1,4)
-    barco2_1 = Barco(2,orientacion)
+    barco2_1 = clases.Barco(2,orientacion)
     orientacion = random.randint(1,4)
-    barco2_2 = Barco(2,orientacion)
+    barco2_2 = clases.Barco(2,orientacion)
     orientacion = random.randint(1,4)
-    barco2_3 = Barco(2,orientacion)
+    barco2_3 = clases.Barco(2,orientacion)
     orientacion = random.randint(1,4)
-    barco3 = Barco(3,orientacion)
+    barco3 = clases.Barco(3,orientacion)
     orientacion = random.randint(1,4)
-    barco3_2 = Barco(3,orientacion)
+    barco3_2 = clases.Barco(3,orientacion)
     orientacion = random.randint(1,4)
-    barco4 = Barco(4,orientacion)
-
+    barco4 = clases.Barco(4,orientacion)
+    #Hora de meter coordenadas, para asegurarnos de que no se pisen.
+    #Usaremos un bucle while que solo parara cuando se haya situado el barco.
+    #Es decir, cuando la funcion insertar_barco devuelva 0.
+    #IMPORTANTE: para optimizar la colocacion, primero metemos los barcos mas grandes.
     bucle =1
     while bucle==1:
-
-        barco1.crear_coordenadas()
-        bucle = campo_jugador1.insertar_barco(barco1.coordenadas, barco1.vidas)
-
+        barco4.crear_coordenadas()
+        #Ahora las metemos las coordenadas, si se encuentra una celda ocupada retornara 1.
+        bucle = campo_jugador1.insertar_barco(barco4.coordenadas,barco4.vidas)
+        #Creamos coordenadas, primero asegurandonos que no se salga del tablero.
+    bucle =1
+    #Asi para todos los barcos.
+    while bucle==1:
+        barco3.crear_coordenadas()
+        bucle = campo_jugador1.insertar_barco(barco3.coordenadas,barco3.vidas)
     bucle =1
     while bucle==1:
-        barco1_2.crear_coordenadas()
-        bucle = campo_jugador1.insertar_barco(barco1_2.coordenadas, barco1_2.vidas)
-    bucle =1
-    while bucle==1:
-        barco1_3.crear_coordenadas()
-        bucle = campo_jugador1.insertar_barco(barco1_3.coordenadas, barco1_3.vidas)
-    bucle =1
-    while bucle==1:
-        barco1_4.crear_coordenadas()
-        bucle = campo_jugador1.insertar_barco(barco1_4.coordenadas, barco1_4.vidas)
+        barco3_2.crear_coordenadas()
+        bucle = campo_jugador1.insertar_barco(barco3_2.coordenadas,barco3_2.vidas)
     bucle =1
     while bucle==1:
         barco2_1.crear_coordenadas()
@@ -57,60 +65,67 @@ def crear_barcos_jugador1(campo_jugador1):
         bucle = campo_jugador1.insertar_barco(barco2_3.coordenadas,barco2_3.vidas)
     bucle =1
     while bucle==1:
-        barco3.crear_coordenadas()
-        bucle = campo_jugador1.insertar_barco(barco3.coordenadas,barco3.vidas)
+        barco1.crear_coordenadas()
+        bucle = campo_jugador1.insertar_barco(barco1.coordenadas, barco1.vidas)
     bucle =1
     while bucle==1:
-        barco3_2.crear_coordenadas()
-        bucle = campo_jugador1.insertar_barco(barco3_2.coordenadas,barco3_2.vidas)
+        barco1_2.crear_coordenadas()
+        bucle = campo_jugador1.insertar_barco(barco1_2.coordenadas, barco1_2.vidas)
     bucle =1
     while bucle==1:
-        barco4.crear_coordenadas()
-        bucle = campo_jugador1.insertar_barco(barco4.coordenadas,barco4.vidas)
+        barco1_3.crear_coordenadas()
+        bucle = campo_jugador1.insertar_barco(barco1_3.coordenadas, barco1_3.vidas)
+    bucle =1
+    while bucle==1:
+        barco1_4.crear_coordenadas()
+        bucle = campo_jugador1.insertar_barco(barco1_4.coordenadas, barco1_4.vidas)
+    
 
     return "Jugador 1 Creado con exito."
 
 
 def crear_barcos_jugador2(campo_jugador2):
+    #Lo mismo que la funcion de crear barcos para jugador 1
     orientacion = random.randint(1,4)
-    jugador_2_barco1 = Barco(1,orientacion)
+    jugador_2_barco1 = clases.Barco(1,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco1_2= Barco(1,orientacion)
+    jugador_2_barco1_2= clases.Barco(1,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco1_3= Barco(1,orientacion)
+    jugador_2_barco1_3= clases.Barco(1,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco1_4= Barco(1,orientacion)
+    jugador_2_barco1_4= clases.Barco(1,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco2_1 = Barco(2,orientacion)
+    jugador_2_barco2_1 = clases.Barco(2,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco2_2 = Barco(2,orientacion)
+    jugador_2_barco2_2 = clases.Barco(2,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco2_3 = Barco(2,orientacion)
+    jugador_2_barco2_3 = clases.Barco(2,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco3 = Barco(3,orientacion)
+    jugador_2_barco3 = clases.Barco(3,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco3_2 = Barco(3,orientacion)
+    jugador_2_barco3_2 = clases.Barco(3,orientacion)
     orientacion = random.randint(1,4)
-    jugador_2_barco4 = Barco(4,orientacion)
+    jugador_2_barco4 = clases.Barco(4,orientacion)
+
+    bucle =1
+
+    while bucle==1:
+        jugador_2_barco4.crear_coordenadas()
+        bucle = campo_jugador2.insertar_barco(jugador_2_barco4.coordenadas,jugador_2_barco4.vidas)
 
     bucle =1
     while bucle==1:
+        jugador_2_barco3.crear_coordenadas()
+        bucle = campo_jugador2.insertar_barco(jugador_2_barco3.coordenadas,jugador_2_barco3.vidas)
+    bucle =1
+    while bucle==1:
+        jugador_2_barco3_2.crear_coordenadas()
+        bucle = campo_jugador2.insertar_barco(jugador_2_barco3_2.coordenadas,jugador_2_barco3_2.vidas)
 
+    bucle=1
+    while bucle==1:
         jugador_2_barco1.crear_coordenadas()
         bucle = campo_jugador2.insertar_barco(jugador_2_barco1.coordenadas, jugador_2_barco1.vidas)
-
-    bucle =1
-    while bucle==1:
-        jugador_2_barco1_2.crear_coordenadas()
-        bucle = campo_jugador2.insertar_barco(jugador_2_barco1_2.coordenadas, jugador_2_barco1_2.vidas)
-    bucle =1
-    while bucle==1:
-        jugador_2_barco1_3.crear_coordenadas()
-        bucle = campo_jugador2.insertar_barco(jugador_2_barco1_3.coordenadas, jugador_2_barco1_3.vidas)
-    bucle =1
-    while bucle==1:
-        jugador_2_barco1_4.crear_coordenadas()
-        bucle = campo_jugador2.insertar_barco(jugador_2_barco1_4.coordenadas, jugador_2_barco1_4.vidas)
     bucle =1
     while bucle==1:
         jugador_2_barco2_1.crear_coordenadas()
@@ -125,43 +140,65 @@ def crear_barcos_jugador2(campo_jugador2):
         bucle = campo_jugador2.insertar_barco(jugador_2_barco2_3.coordenadas,jugador_2_barco2_3.vidas)
     bucle =1
     while bucle==1:
-        jugador_2_barco3.crear_coordenadas()
-        bucle = campo_jugador2.insertar_barco(jugador_2_barco3.coordenadas,jugador_2_barco3.vidas)
+        jugador_2_barco1_2.crear_coordenadas()
+        bucle = campo_jugador2.insertar_barco(jugador_2_barco1_2.coordenadas, jugador_2_barco1_2.vidas)
     bucle =1
     while bucle==1:
-        jugador_2_barco3_2.crear_coordenadas()
-        bucle = campo_jugador2.insertar_barco(jugador_2_barco3_2.coordenadas,jugador_2_barco3_2.vidas)
+        jugador_2_barco1_3.crear_coordenadas()
+        bucle = campo_jugador2.insertar_barco(jugador_2_barco1_3.coordenadas, jugador_2_barco1_3.vidas)
     bucle =1
     while bucle==1:
-        jugador_2_barco4.crear_coordenadas()
-        bucle = campo_jugador2.insertar_barco(jugador_2_barco4.coordenadas,jugador_2_barco4.vidas)
+        jugador_2_barco1_4.crear_coordenadas()
+        bucle = campo_jugador2.insertar_barco(jugador_2_barco1_4.coordenadas, jugador_2_barco1_4.vidas)
 
     return "Jugador 2 creado con exito."
 
 
-def disparo(jugador, direccion, coord1,coord2):
-
-    if direccion == 0:
+def disparo(jugador, direccion, coord1,coord2,niebla):
+    """
+    La funcion recive los parametros la clase del jugador correspondiente.
+    la direccion, sera 1 si dispara un jugador y 2 si es la IA.
+    Ademas, se encargara de revisar el tablero.
+    Primero se asegurara de que la zona a la que apuntamos sea valida
+    es decir, que no sea X o _ (agua), si es valida, ejecuta el disparo y actualiza el tablero
+    si no es valido, devuelve 1 debemos probar a introducir otras coordenadas."""
+    if direccion == 1:
+        #Comprobamos si se ha acertado en el tablero, cambiando la O por una X y quitando una vida al jugador.
         if jugador.campo[coord1,coord2] == "O":
             print("GOLPEEEEEEE! TIRA DE NUEVO")
+            time.sleep(1)
             jugador.campo[coord1,coord2] = "X"
-            print(jugador.campo)
+            niebla[coord1,coord2] = "X"
+            print(niebla)
+            #Comprobamos ademas si ya no quedan vidas, para terminar el juego antes de dar un turno extra.
             jugador.vidas_totales -=1
             if jugador.vidas_totales == 0:
                 return 0
             else:
                 return 1
+        #Aqui comrpobamos si la zona es golpeada o agua.
         elif jugador.campo[coord1,coord2] == "X" or jugador.campo[coord1,coord2] == "_":
             print("Zona ya intentada/golpeada")
+            time.sleep(1)
             return 1
         else:
             print("Agua, HAS FALLDO HAHAHAHAHAHAAHAAHAHAHHA")
+            time.sleep(1)
             jugador.campo[coord1,coord2] = "_"
+            niebla[coord1,coord2] = "_"
             return 0
         
-    if direccion == 1:
+
+    #Esta es la parte de la IA, uso sleep para que la ejecucion no sea tan rapida.
+    #Mas que nada, que se note que es mas realista.
+    if direccion == 2:
+        print("Chatgpt esta pensando...")
+        time.sleep(1)
         if jugador.campo[coord1,coord2] == "O":
+            print("TOMAAAAA CHATGPT TE HA DADO, SUFRE!!!!!!")
+            time.sleep(1)
             jugador.campo[coord1,coord2] = "X"
+            niebla[coord1,coord2] = "X"
             jugador.vidas_totales -=1
             if jugador.vidas_totales == 0:
                 return 0
@@ -170,7 +207,8 @@ def disparo(jugador, direccion, coord1,coord2):
         elif jugador.campo[coord1,coord2] == "X" or jugador.campo[coord1,coord2] == "_":
             return 1
         else:
+            print("Chatgpt ha fallado...")
+            time.sleep(1)
             jugador.campo[coord1,coord2] = "_"
+            niebla[coord1,coord2] = "_"
             return 0
-
-
--- hundir la flota/main.py
+++ hundir la flota/main.py
@@ -1,65 +1,51 @@
+#       ----------------------------------------------------------------------------
+#           HUNDIR LA FLOTA. TRABAJO REALIZADO POR ADRIAN NIETO.
+#           EN ESTE ARCHIVO MAIN PY TENGO LAS LLAMADAS A LAS FUNCIONES Y EL BUCLE
+#           QUE MANTIENE FUNCIONANDO EL JUEGO.
+#           BAJA ABAJO PARA VER LO RELACIANDO EN LOS RESPECTIVOS COMENTARIOS.
+
+#Importamos todas las funciones que necesitamos.
 import numpy as np
 import random
+#Solo funcionan si se importan otros py de la siguiente manera.
 from clases import *
 from funciones import *
+import time
+
+#Creamos los 2 jugadores, jugador 1 es siempre persona, jugador 2 sera IA o persona.
+#Para seguir flujo de codigo, ve a clases.py a las clases de jugador
 campo_jugador1 = Tablero_jugador_1()
 campo_jugador2 = Tablero_jugador_2()
+#Creamos los barcos para cada jugador, para seguir el flujo, ve a la funcion crear_barcos
 crear_barcos_jugador1(campo_jugador1)
 crear_barcos_jugador2(campo_jugador2)
+#creamos la niebla de guerra
+niebla = Niebla_de_guerra
+modo_juego = 0
+while modo_juego != 1 and modo_juego != 2:
+    try:
+        modo_juego = int(input("Selecciona 1 para jugar contra la IA, 2 para jugar 1vs1:   "))
+    except:
+        print("SOLO NUMEROS POR FAVOR.")
+#Esto lo pongo para que parezca que se toma su tiempo, y no ponga toda la informacion tan de golpe.
+print("Matando cucarachas...")
+time.sleep(1)
+print("Ensuciando cubiertas para limpiarlas luego...")
+time.sleep(1)
+print("Despertando al leviathan...")
+time.sleep(1)
+print("Finalizando registro de sirenas...")
+time.sleep(1)
+print("finalizado creacion de barcos, comenzando juego...")
+time.sleep(1)
 
-print(campo_jugador1.vidas_totales)
-print(campo_jugador1.campo)
+juego = Juego(modo_juego)
+juego.jugando(campo_jugador1,campo_jugador2,niebla)
+#para debugging, descomentar para ver tu propio tablero.
+#Se vera igualmente si juegas contra la IA, pero no si juegas contra otra persona.
+#print(campo_jugador1.vidas_totales)
+#print(campo_jugador1.campo)
+#print(campo_jugador2.vidas_totales)
+#print(campo_jugador2.campo)
 
-print("ZONA JUGADOR 2")
-print("hola")
-
-print(campo_jugador2.vidas_totales)
-print(campo_jugador2.campo)
-
-
-while True:
-
-    posicion = 0
-    print("INSTRUCCIONES DE COORDENADAS: iNTRODUCE EN FORMATO X,X.")
-    coordenada = []
-    primera = 0
-    segunda = 0
-    check = 1
-    while check == 1:
-        posicion = input("Introduce coordenada: ")
-        coordenada = posicion.split(",")
-        if len(coordenada) == 2:
-            try:
-                if int(coordenada[0]) <= 10 or int(coordenada[1]) >= 0 and int(coordenada[1]) <= 10 or int(coordenada[0]) >= 0:
-                    primera = int(coordenada[0])-1
-                    segunda = int(coordenada[1])-1
-                    check = disparo(campo_jugador2,0,primera,segunda)
-                else:
-                    print("POR FAVOR, INTRODUCE NUMEROS ENTRE 1 y 10")
-            except:
-                print("POR FAVOR, INTRODUCE NUMEROS ENTRE 1 y 10")
-        else:
-            print("POR FAVOR, INTRODUCE NUMEROS EN FORMATO X,X")
-    if campo_jugador2.vidas_totales == 0:
-        print(campo_jugador1.vidas_totales)
-        print(campo_jugador1.campo)
-        print(campo_jugador2.vidas_totales)
-        print(campo_jugador2.campo)
-        print("JUGADOR1 HA GANADO!!!")
-        break
-    print("Le toca a la IA...")
-    check = 1
-    while check == 1:
-
-        primera = random.randint(0,9)
-        segunda = random.randint(0,9)
-        check = disparo(campo_jugador1,1,primera,segunda)
-    print("Turno de IA finalizado, imrpimiendo tablero...")
-    print(campo_jugador1.vidas_totales)
-    print(campo_jugador1.campo)
-    print(campo_jugador2.vidas_totales)
-    print(campo_jugador2.campo)
-    if campo_jugador1.vidas_totales == 0:
-        print("JUGADOR 2 HA GANADO!!!!")
-        break
 
